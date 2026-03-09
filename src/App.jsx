22d6cf0ba6133387d24d04303f9f84e730b093ec
tosianium <n0time4udawg@gmail.com>
2026-03-09 09:54:04 +0100

    quizlist attempt 1
    main view

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import QuizList from "./components/QuizList";
+import QuizCreator from "./components/QuizCreator";
+import QuizPlayer from "./components/QuizPlayer";
+import Results from "./components/Results";
 
 function App() {
-
   return (
-    <>
-      <div>
+    <BrowserRouter>
+      <div className="p-6 max-w-3xl mx-auto">
+
+        <Routes>
+          <Route path="/" element={<QuizList />} />
+          <Route path="/create" element={<QuizCreator />} />
+          <Route path="/quiz/:id" element={<QuizPlayer />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+
       </div>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/QuizList.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getQuizzes, saveQuizzes } from "../services/storage";
+
+function QuizList() {
+
+  const [quizzes, setQuizzes] = useState([]);
+
+  useEffect(() => {
+    setQuizzes(getQuizzes());
+  }, []);
+
+  const deleteQuiz = (id) => {
+
+    const updated = quizzes.filter(q => q.id !== id);
+    setQuizzes(updated);
+    saveQuizzes(updated);
+
+  };
+
+  return (
+    <div>
+      <h1>Quizy</h1>
+
+      <div className="mb-6 flex gap-3">
+        <Link to="/create">
+          Nowy quiz
+        </Link>
+
+        <Link to="/results">
+          Wyniki
+        </Link>
+      </div>
+
+      {quizzes.map(q => (
+        <div
+          key={q.id}>
+
+          <h3>{q.title}</h3>
+
+          <p>{q.description}</p>
+
+          <div>
+
+            <Link
+              to={`/quiz/${q.id}`}>
+              Rozwiąż
+            </Link>
+
+            <button
+              onClick={() => deleteQuiz(q.id)}>
+              Usuń
+            </button>
+
+          </div>
+
+        </div>
+      ))}
+
+    </div>
+  );
+}
+
+export default QuizList;
