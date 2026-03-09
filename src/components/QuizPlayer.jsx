63107c58e71f925a3dc58193d17f7d29bfab15b7
tosianium <n0time4udawg@gmail.com>
2026-03-09 11:08:46 +0100

    quizplayer update
    attempt on quiz player

---
+++ src/components/QuizPlayer.jsx
@@ -0,0 +1,77 @@
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { getQuizzes, getResults, saveResults } from "../services/storage";
+
+function QuizPlayer() {
+
+  const { id } = useParams();
+
+  const quiz = getQuizzes().find(q => q.id == id);
+
+  const [answers, setAnswers] = useState({});
+
+  const submitQuiz = () => {
+
+    let score = 0;
+
+    quiz.questions.forEach(q => {
+
+      if (answers[q.id] === q.correct) {
+        score++;
+      }
+
+    });
+
+    const percent = Math.round((score / quiz.questions.length) * 100);
+
+    const results = getResults();
+
+    results.push({
+      id: Date.now(),
+      quizId: quiz.title,
+      date: new Date().toLocaleString(),
+      score,
+      total: quiz.questions.length,
+      percent
+    });
+
+    saveResults(results);
+
+    alert(`Twój wynik: ${percent}%`);
+
+  };
+
+  return (
+    <div>
+
+      <h1>{quiz.title}</h1>
+
+      {quiz.questions.map((q, index) => (
+        <div key={q.id}>
+
+          <p>{index + 1}. {q.question}</p>
+
+          {q.options.map((opt, i) => (
+            <label key={i}>
+              <input
+                type="radio"
+                name={q.id}
+                onChange={() =>
+                  setAnswers({ ...answers, [q.id]: i })
+                }/>
+                
+              {opt}
+
+            </label>
+          ))}
+
+        </div>
+      ))}
+
+      <button onClick={submitQuiz}>Zakończ quiz</button>
+
+    </div>
+  );
+}
+
+export default QuizPlayer;
