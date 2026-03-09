e3cf2735f489448c65a362b4872eae95d22f352d
mikeyGregory <szymongrzegorczyk2007@gmail.com>
2026-03-09 11:13:34 +0100

    starage v2
    storage

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,7 +1,15 @@
 export const getQuizzes = () => {
   return JSON.parse(localStorage.getItem("quizzes")) || [];
 };
-
+ 
 export const saveQuizzes = (quizzes) => {
   localStorage.setItem("quizzes", JSON.stringify(quizzes));
+};
+ 
+export const getResults = () => {
+  return JSON.parse(localStorage.getItem("results")) || [];
+};
+ 
+export const saveResults = (results) => {
+  localStorage.setItem("results", JSON.stringify(results));
 };
