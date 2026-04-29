c1b0d707dd84691491cf7b439f0ec36b21eb8d46
mikeyGregory <szymongrzegorczyk2007@gmail.com>
2026-04-29 23:16:59 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import QuizList from "./components/QuizList";
 import QuizCreator from "./components/QuizCreator";
 import QuizPlayer from "./components/QuizPlayer";
 import Results from "./components/Results";
+import bg from "./assets/bgs1.webp";
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <BrowserRouter>
     <div className="h-239 w-full bg-center bg-cover bg-[url(assets/bgs1.webp)]">
       <div className="p-6 max-w-3xl mx-auto">
@@ -17,8 +19,22 @@ function App() {
           <Route path="/results" element={<Results />} />
         </Routes>
 
+=======
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <div
+        className="h-239 w-full bg-center bg-cover"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="p-6 max-w-3xl mx-auto">
+          <Routes>
+            <Route path="/" element={<QuizList />} />
+            <Route path="/create" element={<QuizCreator />} />
+            <Route path="/quiz/:id" element={<QuizPlayer />} />
+            <Route path="/results" element={<Results />} />
+          </Routes>
+        </div>
+>>>>>>> Stashed changes
       </div>
-    </div>
     </BrowserRouter>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -12,4 +11,9 @@ export default defineConfig({
     }),
     tailwindcss()
   ],
+<<<<<<< Updated upstream
 })
+=======
+  base: '/quizyGG/'
+})
+>>>>>>> Stashed changes
